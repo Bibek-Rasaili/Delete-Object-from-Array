c61cff97262e66eb585f126e6b16b9c041aeea71
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-03-23 16:13:04 +0000

    object array implemented.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 // This project will test how to delete an object from an array
 
-console.log("working");
+console.log("---- starts ---");
+
+var comments = [
+  {
+    id : "c1",
+    body: "Hello"
+  },
+  {
+    id : "c2",
+    body: "Hi"
+  },
+  {
+    id : "c3",
+    body: "how are you doing?"
+  },
+  {
+    id : "c4",
+    body: "I am doing fine thank you, yourself?"
+  },
+  {
+    id : "c5",
+    body: "nice to meet you."
+  }
+];
+
+console.log(comments.length);
